d4a15aa0ded717df7d32b1758cea59dccbf46565
DoPiYm <135793791+DoPiYm@users.noreply.github.com>
2023-06-23 12:17:38 +0700

    Example005

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(1, 777); // 1 2 3 ... 776
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 777);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
