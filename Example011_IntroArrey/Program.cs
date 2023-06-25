7b00eb06830331e6207008cad89038de1177cbf0
DoPiYm <135793791+DoPiYm@users.noreply.github.com>
2023-06-25 11:12:37 +0700

    опять шляпа у препода (правильно)а у учеников нет,топ)!

---
+++ Example011_IntroArrey/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int[] array = { 11, 12, 31, 41, 15, 61, 17, 8211, 19 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
